15b05a65845f5b28df3a5b570f1b0550894c4e0e
Marian4 <Mraes.Mariana@gmail.com>
2017-05-11 19:03:01 -0300

    -

---
+++ js/router/router.js
@@ -0,0 +1,11 @@
+/*app.config(function ($routeProvider, $locationProvider) {
+
+    // Remover a exclamação (!) da URL
+    var SEM_PREFIXO = '';
+    $locationProvider.hashPrefix(SEM_PREFIXO);
+
+    $routeProvider
+        .when("/", {
+            templateUrl: "home.html"
+        })
+});*/
