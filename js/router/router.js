d34dd0a03e3a2a5cc05608c8a63c0995acee5e06
Marian4 <Mraes.Mariana@gmail.com>
2017-05-23 22:53:19 -0300

    -

--- js/router/router.js
+++ js/router/router.js
@@ -1,4 +1,4 @@
-/*app.config(function ($routeProvider, $locationProvider) {
+app.config(function ($routeProvider, $locationProvider) {
 
     // Remover a exclamação (!) da URL
     var SEM_PREFIXO = '';
@@ -8,4 +8,5 @@
         .when("/", {
             templateUrl: "home.html"
         })
-});*/
+});
+/*acho q pra fazer com o da avaliação vc tem q separar o css dele e tbm tirar a ag html e deixar só o conteúdo (y)*/
